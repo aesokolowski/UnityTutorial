e05da083e88978f2638ab3a02cceb4aa961a96f4
Andy Sokolowski <44684409+aesokolowski@users.noreply.github.com>
2020-03-25 13:53:49 -0400

    initial commit

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Manager : MonoBehaviour
+{
+    public static Manager instance;
+    int score;
+    public Text scoreText;
+    public GameObject startUI;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GameStart()
+    {
+        startUI.SetActive(false);
+        scoreText.gameObject.SetActive(true);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void ScoreUp()
+    {
+        score++;
+        scoreText.text = score.ToString();
+    }
+}
